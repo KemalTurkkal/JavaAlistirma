76874bb889dcf18409f6c818737af66f2e405829
KemalTurkkal <kemalturkkal58@gmail.com>
2022-12-18 00:55:43 +0300

    Create HarmonikSayilariBulma.java

---
+++ JAVA/HarmonikSayilariBulma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HarmonikSayilariBulma {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int n;
+      System.out.print("N sayısını giriniz : ");
+      n = input.nextInt();
+      double result = 0.0;
+      for(double i = 1; i<=n; i++){
+        result += (1/i);
+      }
+
+      System.out.print(result);
+
+
+      input.close();  
+    }
+}
